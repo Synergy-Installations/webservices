903588578b11353a2edf20bfbfd850e59604fd80
Elias Cerne <eliascerne@icloud.com>
2025-05-20 13:15:05 +0200

    chore: move de-AT locale to at-AT and create new page /kontakt for form

---
+++ apps/frontend/src/app/[locale]/(landing-page)/kontakt/page.tsx
@@ -0,0 +1,36 @@
+import { ContactBlocks } from "@com.synergy/frontend-ui/ContactBlocks";
+import { ContactCommunity } from "@com.synergy/frontend-ui/ContactCommunity";
+import { Form } from "@com.synergy/frontend-ui/Form";
+import RichText from "@com.synergy/frontend-ui/RichText";
+import { useTranslations } from "next-intl";
+
+export default function Page(): JSX.Element {
+  const t = useTranslations("LandingPage.ContactUs.Header");
+
+  return (
+    <>
+      {/* Demo form */}
+      <section className="relative before:absolute before:inset-0 before:h-80 before:pointer-events-none before:bg-gradient-to-b before:from-slate-100 before:-z-10">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div className="px-4 sm:px-6">
+            {/* Page header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16 overflow-hidden">
+              <h1 className="font-inter-tight text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-500 via-slate-900 to-slate-900 pb-4 break-words break-all hyphens-auto">
+              {t("title")}
+              </h1>
+              <div className="text-lg text-slate-500">
+              <RichText>{(tags) => t.rich("description", tags)}</RichText>
+              </div>
+            </div>
+
+            {/* Form */}
+            <Form />
+          </div>
+        </div>
+      </section>
+
+      <ContactBlocks />
+      <ContactCommunity />
+    </>
+  );
+}

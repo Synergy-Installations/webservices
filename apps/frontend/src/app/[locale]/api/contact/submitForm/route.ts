02988ab6cae57193d360c6c4bf7d30ab436eac64
Elias Cerne <eliascerne@icloud.com>
2025-07-25 17:19:40 +0200

    feat: migrate from sendgrid to resend

--- apps/frontend/src/app/[locale]/api/contact/submitForm/route.ts
+++ apps/frontend/src/app/[locale]/api/contact/submitForm/route.ts
@@ -1,5 +1,5 @@
-import * as sgMail from "@sendgrid/mail";
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
 type EmailFormData = {
   to: string;
@@ -12,60 +12,116 @@ type EmailFormData = {
 export async function POST(req: NextRequest) {
   const { to, message, ...restBody }: EmailFormData = await req.json();
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
-  // console.log("process.env.SENDGRID_API_KEY", process.env.SENDGRID_API_KEY);
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-  const msg = {
-    to: [to, "office@synergiemontagen.eco"], // Change to your recipient
-    from: "office@synergiemontagen.eco", // Change to your verified sender
-    subject: "Ihre Anfrage von Synergiemontagen",
-    text: message,
-    html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
-        <div style="text-align: center; margin-bottom: 20px;">
-          <img src="https://synergy-webservices-assets.b-cdn.net/frontend/landingPage/icons/Flyer%20Synergie%20B2C%20v1.svg" alt="Synergiemontagen Logo" style="max-width: 200px;" />
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "office@synergiemontagen.eco",
+      to: [to, "office@synergiemontagen.eco"],
+      subject: "Ihre Anfrage von Synergiemontagen",
+      html: `
+        <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+          <div style="text-align: center; margin-bottom: 20px;">
+            <img src="https://synergy-webservices-assets.b-cdn.net/frontend/landingPage/icons/Flyer%20Synergie%20B2C%20v1.svg" alt="Synergiemontagen Logo" style="max-width: 200px;" />
+          </div>
+          <h2 style="color: #0CC0DF;">Vielen Dank für Ihre Anfrage mit Synergiemontagen</h2>
+          <p><strong>Name:</strong> ${restBody.formData.name}</p>
+          <p><strong>E-Mail:</strong> ${restBody.formData.email}</p>
+          <p><strong>Tel.:</strong> ${restBody.formData.phone}</p>
+          <p><strong>Produktauswahl:</strong> ${restBody.formData.productSelection}</p>
+          <p><strong>Projektdetails:</strong> ${message}</p>
+          <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;" />
+          <p style="font-size: 0.9em; color: #555;">
+            Wenn Sie weitere Informationen, Details oder Dokumente haben, senden Sie diese bitte an uns. Wir freuen uns darauf, Ihnen zu helfen!
+          </p>
+          <div style="text-align: center; margin-top: 20px;">
+            <p style="font-size: 0.8em; color: #888;">© ${new Date().getFullYear()} Synergiemontagen.eco - Alle Rechte vorbehalten.</p>
+          </div>
         </div>
-        <h2 style="color: #0CC0DF;">Vielen Dank für Ihre Anfrage mit Synergiemontagen</h2>
-        <p><strong>Name:</strong> ${restBody.formData.name}</p>
-        <p><strong>E-Mail:</strong> ${restBody.formData.email}</p>
-        <p><strong>Tel.:</strong> ${restBody.formData.phone}</p>
-        <p><strong>Produktauswahl:</strong> ${restBody.formData.productSelection}</p>
-        <p><strong>Aufmerksam durch:</strong> ${restBody.formData.funnel}</p>
-        <p><strong>Projektdetails:</strong> ${message}</p>
-        <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;" />
-        <p style="font-size: 0.9em; color: #555;">
-          Wenn Sie weitere Informationen, Details oder Dokumente haben, senden Sie diese bitte an uns. Wir freuen uns darauf, Ihnen zu helfen!
-        </p>
-        <div style="text-align: center; margin-top: 20px;">
-          <p style="font-size: 0.8em; color: #888;">© ${new Date().getFullYear()} Synergiemontagen.eco - Alle Rechte vorbehalten.</p>
-        </div>
-      </div>
-    `,
-  };
-
-  let errorBoolean = false;
-
-  await sgMail
-    .send(msg)
-    .then((value) => {})
-    .catch((error) => {
-      errorBoolean = true;
-      console.error("Mail sent unseccussfully", error, error.code);
-      if (error.response) {
-        console.error(error.response.body);
-      }
+      `,
     });
-  return errorBoolean
-    ? new Response(`Email did not send (final) ${errorBoolean}`, {
+    if (error) {
+      return new Response(`Email did not send: ${error}`, {
         status: 500,
         headers: {
           "Content-Type": "application/json",
         },
-      })
-    : new Response(`Email sent successfully (final) ${errorBoolean}`, {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
+    }
+
+    return new Response(`Email sent successfully (final)`, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(`Email did not send (catch): ${error}`, {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  // return new Response(`Email sent successfully (final)`, {
+  //   status: 200,
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+
+  // const msg = {
+  //   to: [to, "office@synergiemontagen.eco"], // Change to your recipient
+  //   from: "office@synergiemontagen.eco", // Change to your verified sender
+  //   subject: "Ihre Anfrage von Synergiemontagen",
+  //   text: message,
+  //   html: `
+  //     <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+  //       <div style="text-align: center; margin-bottom: 20px;">
+  //         <img src="https://synergy-webservices-assets.b-cdn.net/frontend/landingPage/icons/Flyer%20Synergie%20B2C%20v1.svg" alt="Synergiemontagen Logo" style="max-width: 200px;" />
+  //       </div>
+  //       <h2 style="color: #0CC0DF;">Vielen Dank für Ihre Anfrage mit Synergiemontagen</h2>
+  //       <p><strong>Name:</strong> ${restBody.formData.name}</p>
+  //       <p><strong>E-Mail:</strong> ${restBody.formData.email}</p>
+  //       <p><strong>Tel.:</strong> ${restBody.formData.phone}</p>
+  //       <p><strong>Produktauswahl:</strong> ${restBody.formData.productSelection}</p>
+  //       <p><strong>Aufmerksam durch:</strong> ${restBody.formData.funnel}</p>
+  //       <p><strong>Projektdetails:</strong> ${message}</p>
+  //       <hr style="border: none; border-top: 1px solid #ddd; margin: 20px 0;" />
+  //       <p style="font-size: 0.9em; color: #555;">
+  //         Wenn Sie weitere Informationen, Details oder Dokumente haben, senden Sie diese bitte an uns. Wir freuen uns darauf, Ihnen zu helfen!
+  //       </p>
+  //       <div style="text-align: center; margin-top: 20px;">
+  //         <p style="font-size: 0.8em; color: #888;">© ${new Date().getFullYear()} Synergiemontagen.eco - Alle Rechte vorbehalten.</p>
+  //       </div>
+  //     </div>
+  //   `,
+  // };
+
+  // let errorBoolean = false;
+
+  // await sgMail
+  //   .send(msg)
+  //   .then((value) => {})
+  //   .catch((error) => {
+  //     errorBoolean = true;
+  //     console.error("Mail sent unseccussfully", error, error.code);
+  //     if (error.response) {
+  //       console.error(error.response.body);
+  //     }
+  //   });
+  // return errorBoolean
+  //   ? new Response(`Email did not send (final) ${errorBoolean}`, {
+  //       status: 500,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     })
+  //   : new Response(`Email sent successfully (final) ${errorBoolean}`, {
+  //       status: 200,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
 }
--- apps/frontend/src/app/[locale]/api/contact/submitFunnel/route.ts
+++ apps/frontend/src/app/[locale]/api/contact/submitFunnel/route.ts
@@ -1,5 +1,6 @@
 import * as sgMail from "@sendgrid/mail";
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
 type EmailFormData = {
   to: string;
@@ -22,7 +23,8 @@ export async function POST(req: NextRequest) {
   const { to, message, submitId, ...restBody }: EmailFormData =
     await req.json();
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  // sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
   // console.log(
   //   "process.env.SENDGRID_API_KEY",
   //   process.env.SENDGRID_API_KEY,
@@ -30,31 +32,32 @@ export async function POST(req: NextRequest) {
   //   message,
   //   restBody
   // );
-  console.log("restBody", restBody);
+  // console.log("restBody", restBody);
 
-  const msg = {
-    to: [to, "office@synergiemontagen.eco"], // Change to your recipient
-    from: "office@synergiemontagen.eco", // Change to your verified sender
-    subject: "Ihre Anfrage von Synergiemontagen",
-    text: "Ihre Anfrage wurde erfolgreich versendet",
-    html: `
-<html>
-<head>
-  <style>
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "office@synergiemontagen.eco",
+      to: [to, "office@synergiemontagen.eco"],
+      subject: "Ihre Anfrage von Synergiemontagen",
+      html: `
+  <html>
+  <head>
+    <style>
     body {
       font-family: Arial, sans-serif;
       margin: 0;
       padding: 0;
       background-color: #f4f4f4;
-      color: #333;
+      color: #000;
     }
     .email-container {
       max-width: 600px;
       margin: 20px auto;
-      background: #333;
+      background: #fff;
       border-radius: 8px;
       overflow: hidden;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      color: #000;
     }
     .email-header {
       background-color: #efefef;
@@ -66,31 +69,36 @@ export async function POST(req: NextRequest) {
     }
     .email-body {
       padding: 20px;
+      color: #000;
     }
     .email-title {
       font-size: 24px;
       font-weight: bold;
       margin-bottom: 10px;
       text-align: center;
+      color: #000;
     }
     .email-description {
       font-size: 16px;
       margin-bottom: 20px;
       text-align: center;
+      color: #000;
     }
     table {
       width: 100%;
       border-collapse: collapse;
       margin-bottom: 20px;
+      color: #000;
     }
     table th, table td {
       border: 1px solid #ddd;
       padding: 8px;
       text-align: left;
+      color: #000;
     }
     table th {
       background-color: #0cc0df;
-      color: white;
+      color: #fff;
     }
     .email-button-top {
       margin-bottom: 20px;
@@ -119,123 +127,77 @@ export async function POST(req: NextRequest) {
       padding: 10px;
       background-color: #f4f4f4;
     }
-  </style>
-</head>
-<body>
-  <div class="email-container">
+    </style>
+  </head>
+  <body>
+    <div class="email-container">
     <div class="email-header">
       <img src="https://synergy-webservices-assets.b-cdn.net/frontend/landingPage/icons/Flyer%20Synergie%20B2C%20v1.svg" alt="Synergiemontagen Logo" />
     </div>
     <div class="email-body">
       <h2 class="email-title">Vielen Dank für Ihre Anfrage mit Synergiemontagen</h2>
       <p class="email-description">
-        Wir haben Ihre Anfrage erfolgreich erhalten. Sie können jetzt Ihr Dashboard besuchen, um den Status Ihrer Anfrage zu überprüfen, oder mit unserem Team kommunizieren, falls Sie weitere Fragen haben.
+      Wir haben Ihre Anfrage erfolgreich erhalten. Sie können jetzt Ihr Dashboard besuchen, um den Status Ihrer Anfrage zu überprüfen, oder mit unserem Team kommunizieren, falls Sie weitere Fragen haben.
       </p>
       <div class="email-button-top">
-        <a href="${submitId ? `https://synergiemontagen.eco/dashboard/submits/${submitId}/steps` : "https://synergiemontagen.eco/dashboard"}" class="button">Zum Dashboard</a>
+      <a href="${submitId ? `https://synergiemontagen.eco/dashboard/submits/${submitId}/steps` : "https://synergiemontagen.eco/dashboard"}" class="button">Zum Dashboard</a>
       </div>
       <table>
-        <thead>
-          <tr>
-            <th>Frage</th>
-            <th>Ausgewählt</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${restBody.formData
-            .map((form) =>
-              form.forms
-                .map(
-                  (f) => `
-          <tr>
-            <td>${f.formTitle}</td>
-            <td>${Array.isArray(f.selected) ? f.selected.join(", ") : f.selected}</td>
-          </tr>
-        `
-                )
-                .join("")
-            )
-            .join("")}
-        </tbody>
+      <thead>
+        <tr>
+        <th>Frage</th>
+        <th>Ausgewählt</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${restBody.formData
+        .map((form) =>
+          form.forms
+          .map(
+            (f) => `
+        <tr>
+        <td>${f.formTitle}</td>
+        <td>${Array.isArray(f.selected) ? f.selected.join(", ") : f.selected}</td>
+        </tr>
+      `
+          )
+          .join("")
+        )
+        .join("")}
+      </tbody>
       </table>
       <div class="email-button-bottom">
-        <a href="${submitId ? `https://synergiemontagen.eco/dashboard/submits/${submitId}/steps` : "https://synergiemontagen.eco/dashboard"}" class="button">Zum Projekt</a>
+      <a href="${submitId ? `https://synergiemontagen.eco/dashboard/submits/${submitId}/steps` : "https://synergiemontagen.eco/dashboard"}" class="button">Zum Projekt</a>
       </div>
     </div>
     <div class="email-footer">
       © ${new Date().getFullYear()} Synergiemontagen.eco - Alle Rechte vorbehalten.
     </div>
-  </div>
-</body>
-</html>`,
-  };
-
-  let errorBoolean = false;
-  // console.log("msg", msg);
-  // try {
-  await sgMail
-    .send(msg)
-    .then((value) => {
-      // console.log("Mail sent successfully");
-      // console.log("then", value[0].body);
-      // Does not get invoked
-      // return new Response("Email sent successfully (then)", {
-      //   status: 200,
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      // });
-    })
-    .catch((error) => {
-      errorBoolean = true;
-      console.error("Mail sent unseccussfully", error, error.code);
-      if (error.response) {
-        console.error(error.response.body);
-      }
-      // Does not get invoked
-      // if (error.code != 200)
-      //   return new Response("Email did not send (catch)", {
-      //     status: 500,
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //   });
-      // else
-      //   // return NextResponse.json({
-      //   //   status: error.code,
-      //   //   message: "Email did not send",
-      //   // });
-      //   return new Response("Email sent successfully (catch)", {
-      //     status: 200,
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //   });
+    </div>
+  </body>
+  </html>`,
     });
-  // return NextResponse.json({
-  //   message: "Email sent successfully (catch)",
-  // });
-  // } catch (error) {
-  //   errorBoolean = true;
-  //   console.error("error", error);
-  //   return NextResponse.json({
-  //     status: 500,
-  //     message: "Email did not send catch",
-  //   });
-  // }
-
-  // console.log("errorBoolean", errorBoolean);
-  return errorBoolean
-    ? new Response(`Email did not send (final) ${errorBoolean}`, {
+    if (error) {
+      return new Response(`Email did not send: ${error}`, {
         status: 500,
         headers: {
           "Content-Type": "application/json",
         },
-      })
-    : new Response(`Email sent successfully (final) ${errorBoolean}`, {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
+    }
+
+    return new Response(`Email sent successfully (final)`, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(`Email did not send (catch): ${error}`, {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }

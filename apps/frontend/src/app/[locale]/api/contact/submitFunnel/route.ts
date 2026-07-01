d03b4635ad4736f430f261d59d792050f033e81d
Elias Cerne <eliascerne@icloud.com>
2026-07-01 15:13:07 +0200

    feat: change e-mail submit addresses

--- apps/frontend/src/app/[locale]/api/contact/submitFunnel/route.ts
+++ apps/frontend/src/app/[locale]/api/contact/submitFunnel/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: NextRequest) {
   try {
     const { data, error } = await resend.emails.send({
       from: "office@synergie.cc",
-      to: [to],
+      to: [to, "office@synergie.cc"],
       subject: "Ihre Anfrage von Synergiemontagen",
       html: `
   <html>
